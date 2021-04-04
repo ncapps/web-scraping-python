a103fc45d86567548387f2e7a36d5bfcf09efd3d
Nick Capps <nickcapps10@gmail.com>
2021-04-04 01:31:37 +0000

    Use PWD for venv

--- scripts/postCreateCommand.sh
+++ scripts/postCreateCommand.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-WORKSPACE=/workspaces/web-scraping-python
 
 # Create python virtual env and install packages
-virtualenv $WORKSPACE/venv
-source $WORKSPACE/venv/bin/activate
-pip3 install -r $WORKSPACE/requirements.txt
+virtualenv $PWD/venv
+source $PWD/venv/bin/activate
+pip3 install -r $PWD/requirements.txt
